02c8fed6a2c9f1d6be3b34dc58286e7d7851bfa1
MukhammadKhojiAkbar Khusanov <74467533+khusanov-m@users.noreply.github.com>
2021-05-08 09:26:25 +0500

    Add files via upload

--- 03 Developer-Skills/script.js
+++ 03 Developer-Skills/script.js
@@ -16,16 +16,21 @@ into sub-problems!
 Test data:
 
 § Data 1: [17, 21, 23]
-§ Data 2: [12, 5, -5, 0, 4]*/
+§ Data 2: [12, 5, -5, 0, 4]
+*/
 
 // Solution to the challendge #1
 
-const arr = [17, 21, 23];
+const data1 = [17, 21, 23];
+const data2 = [12, 5, -5, 0, 4];
 
-const printForecast = function () {
+const printForecast = function (arr) {
+  let str = "";
   for (let i = 0; i < arr.length; i++) {
-    console.log(`${arr[i]}ºC in ${i+1} days`);
+    str += `${arr[i]}ºC in ${i + 1} days ... `;
   }
+  console.log("... " + str);
 };
 
-printForecast();
+printForecast(data1);
+printForecast(data2);
